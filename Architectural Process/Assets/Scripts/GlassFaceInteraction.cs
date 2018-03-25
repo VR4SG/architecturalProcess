e9b2491fe4694f4046030666630dbd52eeaddfe2
Cesar Gonzalez <cecrigope@hotmail.com>
2018-03-25 14:44:51 -0400

    Glasses disappear when placed close to face

---
+++ Architectural Process/Assets/Scripts/GlassFaceInteraction.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlassFaceInteraction : MonoBehaviour {
+
+    public GameObject Glasses;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Glasses")
+        {
+            Glasses.SetActive(false);
+        }
+    }
+
+
+}
