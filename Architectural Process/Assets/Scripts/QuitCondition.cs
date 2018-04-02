6868f3863229236ff4b50ae63e31ed01a4223354
Cesar Gonzalez <cecrigope@hotmail.com>
2018-04-02 18:06:46 -0400

    Able to remove glasses, began working on hinges on doors

---
+++ Architectural Process/Assets/Scripts/QuitCondition.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitCondition : MonoBehaviour {
+
+    public GameObject Condition;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (OVRInput.GetDown(OVRInput.Button.Four))
+        {
+            Debug.Log("BUTTON FOUR PRESSED");
+            Condition.SetActive(false);
+        }
+    }
+}
