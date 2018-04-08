ca81cb78da9a7a00ad17e5b2096941e1ec0f4bca
michaeltngo <michaeltngo11@gmail.com>
2018-04-08 15:25:19 -0400

    glaucoma trigger

---
+++ Architectural Process/Assets/Scripts/GlaucomaInteraction.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.ImageEffects;
+
+public class GlaucomaInteraction : MonoBehaviour {
+
+    public GameObject Glasses;
+    public VignetteAndChromaticAberration vig;
+
+    void OnTriggerEnter(Collider col)
+    {
+
+        if (col.gameObject.tag == "Glaucoma")
+        {
+            DestroyObject(Glasses);
+            vig.enabled = true;
+        }
+    }
+
+}
