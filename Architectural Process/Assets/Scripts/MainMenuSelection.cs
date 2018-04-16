9fb2727016dd62f53754c1d787b7a3c54351a08c
Cesar Gonzalez <cecrigope@hotmail.com>
2018-04-16 04:23:30 -0400

    Basic design on menu done

---
+++ Architectural Process/Assets/Scripts/MainMenuSelection.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuSelection : MonoBehaviour {
+
+    public void OnLoadStart()
+    {
+        //SceneManager.LoadScene("selection_ui", LoadSceneMode.Single);
+        Debug.Log("START");
+    }
+    public void OnLoadMoreInfo()
+    {
+        //SceneManager.LoadScene("selection_raw", LoadSceneMode.Single);
+        Debug.Log("MORE INFO");
+    }
+    public void OnLoadCredits()
+    {
+        //SceneManager.LoadScene("selection_physics", LoadSceneMode.Single);
+        Debug.Log("CREDITS");
+    }
+    public void OnLoadQuit()
+    {
+        //SceneManager.LoadScene("selection_all", LoadSceneMode.Single);
+        Debug.Log("QUIT");
+    }
+}
