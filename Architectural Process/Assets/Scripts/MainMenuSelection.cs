8c5bde849c7ca39a631ecdce058fd0c3179b4b73
Cesar Gonzalez <cecrigope@hotmail.com>
2018-04-17 04:39:48 -0400

    Added Learn More placeholders

--- Architectural Process/Assets/Scripts/MainMenuSelection.cs
+++ Architectural Process/Assets/Scripts/MainMenuSelection.cs
@@ -3,8 +3,15 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuSelection : MonoBehaviour {
 
-    public GameObject Credits;
-    public GameObject InfoBlueCircle;
+    public GameObject RedSquareCredits;
+    public GameObject RedSquareMainMenu;
+    public GameObject BlueCircleLearnMore;
+    public GameObject BlueCircleMainMenu;
+
+    public GameObject RedSquareCataracts;
+    public GameObject RedSquareDiabetic;
+    public GameObject RedSquareGlaucoma;
+    public GameObject RedSquareMacular;
 
     public void OnLoadStart()
     {
@@ -13,17 +20,68 @@ public class MainMenuSelection : MonoBehaviour {
     }
     public void OnLoadMoreInfo()
     {
-        InfoBlueCircle.SetActive(true);
+        BlueCircleLearnMore.SetActive(true);
+        BlueCircleMainMenu.SetActive(false);
         Debug.Log("MORE INFO");
     }
     public void OnLoadCredits()
     {
-        Credits.SetActive(true);
+        RedSquareCredits.SetActive(true);
+        RedSquareMainMenu.SetActive(false);
         Debug.Log("CREDITS");
     }
     public void OnLoadQuit()
     {
         //SceneManager.LoadScene("selection_all", LoadSceneMode.Single);
         Debug.Log("QUIT");
+    }
+
+    public void OnLoadCataracts()
+    {
+        RedSquareCataracts.SetActive(true);
+        RedSquareDiabetic.SetActive(false);
+        RedSquareGlaucoma.SetActive(false);
+        RedSquareMacular.SetActive(false);
+        RedSquareMainMenu.SetActive(false);
+    }
+
+    public void OnLoadDiabetic()
+    {
+        RedSquareCataracts.SetActive(false);
+        RedSquareDiabetic.SetActive(true);
+        RedSquareGlaucoma.SetActive(false);
+        RedSquareMacular.SetActive(false);
+        RedSquareMainMenu.SetActive(false);
+    }
+
+    public void OnLoadGlaucoma()
+    {
+        RedSquareCataracts.SetActive(false);
+        RedSquareDiabetic.SetActive(false);
+        RedSquareGlaucoma.SetActive(true);
+        RedSquareMacular.SetActive(false);
+        RedSquareMainMenu.SetActive(false);
+    }
+
+    public void OnLoadMacular()
+    {
+        RedSquareCataracts.SetActive(false);
+        RedSquareDiabetic.SetActive(false);
+        RedSquareGlaucoma.SetActive(false);
+        RedSquareMacular.SetActive(true);
+        RedSquareMainMenu.SetActive(false);
+    }
+
+    public void OnLoadBack()
+    {
+        RedSquareCataracts.SetActive(false);
+        RedSquareDiabetic.SetActive(false);
+        RedSquareGlaucoma.SetActive(false);
+        RedSquareMacular.SetActive(false);
+        RedSquareMainMenu.SetActive(true);
+        RedSquareCredits.SetActive(false);
+
+        BlueCircleLearnMore.SetActive(false);
+        BlueCircleMainMenu.SetActive(true);
     }
 }
