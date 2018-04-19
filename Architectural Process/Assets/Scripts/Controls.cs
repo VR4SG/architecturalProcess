be55cbdb4d5b31ea98308edb3a9b261571f4a522
Cesar Gonzalez <cecrigope@hotmail.com>
2018-04-19 01:48:40 -0400

    Added instructions screen to experience

---
+++ Architectural Process/Assets/Scripts/Controls.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Controls : MonoBehaviour {
+
+    public GameObject Panel;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.Three))
+        {
+            if (Panel.activeSelf) {
+                Panel.SetActive(false);
+            }
+
+            else
+            {
+                Panel.SetActive(true);
+            }
+        }
+
+        if (OVRInput.GetDown(OVRInput.Button.Start))
+        {
+            SceneManager.LoadScene("menu", LoadSceneMode.Single);
+        }
+    }
+}
