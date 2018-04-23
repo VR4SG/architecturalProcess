4fbfe2964cb942ef1d594f0ddd24ec6264ab3522
Cesar Gonzalez <cecrigope@hotmail.com>
2018-04-23 03:48:22 -0400

    Added mesh colliders to everything in spec scene

---
+++ Architectural Process/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour {
+
+	void OnTriggerEnter (Collider col) {
+		if (col.gameObject.tag == "Room2")
+        {
+            Debug.Log("CHANGE INTO SPEC STORE");
+            SceneManager.LoadScene("SPEC", LoadSceneMode.Single);
+        }
+	}
+}
