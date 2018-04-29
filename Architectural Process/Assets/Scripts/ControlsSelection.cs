280ad3587986f471ef864ca1528f74fed58f802a
Cesar Gonzalez <cecrigope@hotmail.com>
2018-04-29 03:25:58 -0400

    Added tutorial for quickFilter feature

--- Architectural Process/Assets/Scripts/ControlsSelection.cs
+++ Architectural Process/Assets/Scripts/ControlsSelection.cs
@@ -3,13 +3,31 @@ using UnityEngine.SceneManagement;
 
 public class ControlsSelection : MonoBehaviour {
 
-    public void OnLoadContinue()
-    {
-        SceneManager.LoadScene("Office", LoadSceneMode.Single);
-    }
+    public GameObject controllersPanel;
+    public GameObject quickFilterPanel;
 
     public void OnLoadMainMenu()
     {
         SceneManager.LoadScene("Menu", LoadSceneMode.Single);
     }
+
+    public void OnLoadContinue()
+    {
+        
+        quickFilterPanel.SetActive(true);
+        controllersPanel.SetActive(false);
+    }
+
+    public void OnLoadBack()
+    {
+        controllersPanel.SetActive(true);
+        quickFilterPanel.SetActive(false);
+    }
+
+    public void onLoadOffice()
+    {
+        SceneManager.LoadScene("Office", LoadSceneMode.Single);
+    }
+
+
 }
