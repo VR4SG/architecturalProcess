2872c46a7e767df44f25368e2b49ad53be6e07aa
Cesar Gonzalez <cecrigope@hotmail.com>
2018-04-30 04:28:06 -0400

    Trash can collider

---
+++ Architectural Process/Assets/Scripts/EasterEgg.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EasterEgg : MonoBehaviour {
+
+	// Update is called once per frame
+	void OnTriggerEnter (Collider col) {
+		if(col.gameObject.tag == "Pear")
+        {
+            // SceneManager.LoadScene("", LoadSceneMode.Single);
+            Debug.Log("EASTER EGG!");
+        }
+	}
+}
