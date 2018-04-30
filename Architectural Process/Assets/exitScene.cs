0d4b8d3f247f972c90e9696796390e1bd6d09ed1
Cesar Gonzalez <cecrigope@hotmail.com>
2018-04-30 07:49:47 -0400

    Exiting scenes back to office works

---
+++ Architectural Process/Assets/exitScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class exitScene : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Debug.Log("EXIT");
+            SceneManager.LoadScene("Office", LoadSceneMode.Single);
+        }
+    }
+}
